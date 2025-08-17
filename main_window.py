7f70d7068611c6f7c79169c2c993bc18049727a7
Claudio-NG <claudiopatrickcp@gmail.com>
2025-08-17 15:58:56 -0300

    atualização

--- main_window.py
+++ main_window.py
@@ -14,7 +14,6 @@ from base import BaseWindow
 from config import cfg_get
 from combustivel import CombustivelWindow
 
-
 class CenarioGeralWindow(QWidget):
     def __init__(self):
         super().__init__()
